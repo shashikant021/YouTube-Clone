fbe9f2a4a5433e1f32a74a00186263a8e5f393d6
shashikant021 <shashikant.sahu.13768229@gmail.com>
2025-06-10 22:28:40 +0530

    header style done for mobile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,13 +20,13 @@ function Header({ toggleSidebar }) {
   return (
     <>
       <header className="flex items-center justify-between px-3 md:px-6 py-3 bg-white shadow-md sticky top-0 z-100">
-        <div className="flex item-center gap-2 md:gap-4">
+        <div className="flex item-center gap-1 sm:gap-2 md:gap-4">
           <button onClick={toggleSidebar} className="text-2xl">
             <HiMenu />
           </button>
           <Link
             to="/"
-            className=" flex items-center gap-1 text-xl font-bold text-shadow-lg"
+            className=" flex items-center sm:gap-1 text-xl font-bold text-shadow-lg"
           >
             <IoLogoYoutube className="text-2xl text-red-600" />
             YouTube
@@ -51,10 +51,10 @@ function Header({ toggleSidebar }) {
         </div>
 
         {/* Auth Section & Mobile Search Toggle */}
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-1 sm:gap-3">
           {/* Mobile Search Toggle */}
           <button
-            className="sm:hidden bg-gray-200 px-4 py-1 rounded-xl"
+            className="sm:hidden bg-gray-200 px-2 py-1 rounded-xl"
             onClick={() => setShowMobileSearch((prev) => !prev)}
           >
             <FiSearch className="text-xl" />
@@ -69,12 +69,13 @@ function Header({ toggleSidebar }) {
             </Link>
           ) : (
             <>
-              <span className="flex items-center gap-1 text-sm font-medium text-gray-700 bg-zinc-300 py-1 px-2 md:px-4 rounded">
-                <MdAccountCircle className="text-2xl" /> {user.username}
+              <span className="flex items-center gap-1 text-sm font-medium text-gray-700 bg-zinc-300 py-1 px-1 sm:px-2 md:px-4 rounded">
+                <MdAccountCircle className="text-xl sm:text-2xl" />{" "}
+                {user.username}
               </span>
               <button
                 onClick={logout}
-                className="text-sm font-semibold bg-red-500 hover:bg-red-700 text-white px-2 md:px-3 py-1 rounded-md"
+                className="text-sm font-semibold bg-red-500 hover:bg-red-700 text-white px-1 sm:px-2 md:px-3 py-1 rounded-md"
               >
                 Logout
               </button>
